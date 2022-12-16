fce8a4c56b5c05b1def24fc58fe60401b5aa3f02
Ryszard Fron <ryszard.fron1973@gmail.com>
2022-12-16 23:56:41 +0100

    adding new tasks and showing if a task is done

--- js/script.js
+++ js/script.js
@@ -1,34 +1,62 @@
 {
 
-const tasks = [
+  const tasks = [
     {
-        content: "test1",
-        done: false,
+      content: "test1",
+      done: false,
     },
     {
-        content: "test2",
-        done: true,
+      content: "test2",
+      done: true,
     },
-];
+  ];
 
 
-const render = () => {
-  let htmlString = "";
+  const render = () => {
+    let htmlString = "";
 
-  for (const task of tasks) {
-    htmlString += `
+    for (const task of tasks) {
+      htmlString += `
     <li class="tasksList__toDo--next"
-    ${task.done ? "style=\"text-decoration: line-through\"" : ""}> 
-    <button class="js-done">✔</button>
-    ${task.content} 
-    <button class="js-remove">🗑</button>
+      ${task.done ? "style=\"text-decoration: line-through\"" : ""}>
+      <button class="js-done">✔</button>
+      ${task.content} 
+      <button class="js-remove">🗑</button>
     </li>
     `;
+    };
+
+    document.querySelector(".js-tasks").innerHTML = htmlString;
   };
 
-  document.querySelector(".js-tasks").innerHTML = htmlString;
-}
+  const addNewTask = (newTaskContent) => {
+    tasks.push({
+      content: newTaskContent,
+    });
 
-render()
+    render();
+  };
 
-}
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+
+    const newTaskContent = document.querySelector(".js-newTask").value.trim();
+
+    if (newTaskContent === "") {
+      return;
+    }
+
+    addNewTask(newTaskContent);
+  };
+
+  const init = () => {
+    render();
+
+    const form = document.querySelector(".js-form");
+
+    form.addEventListener("submit", onFormSubmit);
+  };
+
+  init();
+
+};
